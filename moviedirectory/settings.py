279fcd6c0cded981ba4307ce1786341f0fbb6b13
Antonin MATHUBERT <tonin386@gmail.com>
2020-01-23 14:11:58 +0100

    Change default_from_email setting.

--- moviedirectory/settings.py
+++ moviedirectory/settings.py
@@ -40,6 +40,7 @@ EMAIL_HOST = str(config.get('mail', 'host'))
 EMAIL_HOST_USER = str(config.get('mail', 'user'))
 EMAIL_HOST_PASSWORD = str(config.get('mail', 'password'))
 EMAIL_PORT = int(config.get('mail', 'port'))
+DEFAULT_FROM_EMAIL = "\"Movie Directory\" <moviedirectory@fiddlecomputers.fr>"
 
 
 # Application definition
