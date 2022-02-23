306b6a3c80022e73dfb09e7742d036f6bc4391c4
Antonin Mathubert <tonin386@gmail.com>
2022-02-23 18:49:14 +0100

    Updated BS4 to BS5. New search functionnality available in Watchlist.

---
+++ static/js/utilities.js
@@ -0,0 +1,46 @@
+var currentIndex = 0;
+
+function scrollWatchListLeft() {
+
+    console.log(currentIndex);
+
+    var elements = document.getElementsByClassName("infinite-x-child");
+    var scrollSize = 0;
+
+    if (window.outerWidth < 1200) {
+        currentIndex++;
+        if (currentIndex >= elements.length) currentIndex--;
+        scrollSize = elements[currentIndex].getBoundingClientRect().left;
+        console.log(scrollSize);
+    }
+    else {
+        currentIndex += 6;
+        if (currentIndex >= elements.length) currentIndex = elements.length - 1;
+        scrollSize = elements[currentIndex].getBoundingClientRect().left;
+    }
+
+    var el = document.getElementById("watchlist");
+    el.scrollLeft += scrollSize;
+}
+
+function scrollWatchListRight() {
+
+    console.log(currentIndex);
+
+    var elements = document.getElementsByClassName("infinite-x-child");
+    var scrollSize = 0;
+
+    if (window.outerWidth < 1200) {
+        currentIndex--;
+        if (currentIndex < 0) currentIndex++;
+        scrollSize = elements[currentIndex].getBoundingClientRect().left;
+    }
+    else {
+        currentIndex -= 6;
+        if (currentIndex < elements.length) currentIndex = 0;
+        scrollSize += elements[currentIndex].getBoundingClientRect().left;
+    }
+
+    var el = document.getElementById("watchlist");
+    el.scrollLeft += scrollSize;
+}
