ccd170453fbea5209a5ceb60e0b4074b3f2c4d23
Antonin Mathubert <tonin386@gmail.com>
2022-02-24 21:14:56 +0100

    Manual scrolling working on mobile.

--- static/js/utilities.js
+++ static/js/utilities.js
@@ -3,54 +3,80 @@ var el = document.getElementById("autoscrollList");
 var elements = document.getElementsByClassName("infinite-x-child");
 var scrollSize = 0;
 
-function scrollWatchListLeft() {
+if (window.outerWidth >= 1200) {
 
-    if (window.outerWidth < 1200) {
-        currentIndex++;
-        if (currentIndex >= elements.length) currentIndex--;
-        scrollSize = elements[currentIndex].getBoundingClientRect().left;
-        console.log(scrollSize);
-    }
-    else {
-        currentIndex += 6;
-        if (currentIndex >= elements.length) currentIndex = elements.length - 1;
-        scrollSize = elements[currentIndex].getBoundingClientRect().left;
-    }
+    el.onwheel = function (e) {
 
-    el.scrollLeft += scrollSize;
-}
+        if (e.deltaY > 0) {
+            currentIndex++;
+        }
+        else {
+            currentIndex--;
+        }
 
-function scrollWatchListRight() {
-
-    if (window.outerWidth < 1200) {
-        currentIndex--;
-        if (currentIndex < 0) currentIndex++;
-        scrollSize = elements[currentIndex].getBoundingClientRect().left;
-    }
-    else {
-        currentIndex -= 6;
         if (currentIndex < 0) currentIndex = 0;
-        scrollSize = elements[currentIndex].getBoundingClientRect().left;
+        else if (currentIndex >= elements.length - 5) currentIndex = elements.length - 5;
+
+        scrollSize = elements[currentIndex].getBoundingClientRect().left
+        el.scrollLeft += scrollSize;
+    }
+}
+else {
+
+    let touchstartX = 0;
+    let touchendX = 0;
+
+    function handleGesture() {
+        if (touchendX < touchstartX) currentIndex++;
+        if (touchendX > touchstartX) currentIndex--;
+
+        if (currentIndex < 0) currentIndex = 0;
+        else if (currentIndex >= elements.length - 1) currentIndex = elements.length - 1;
+
+        scrollSize = elements[currentIndex].getBoundingClientRect().left
+        el.scrollLeft += scrollSize;
     }
 
-    el.scrollLeft += scrollSize;
+    el.addEventListener('touchstart', e => {
+        touchstartX = e.changedTouches[0].screenX;
+    });
+
+    el.addEventListener('touchend', e => {
+        touchendX = e.changedTouches[0].screenX;
+        handleGesture();
+    });
 }
 
 
-// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
-el.onwheel = function (e) { // or window.addEventListener("scroll"....
-    if(e.deltaY > 0) {
-        currentIndex++;
-    }
-    else {
-        currentIndex--;
-    }
-    if(currentIndex < 0) currentIndex = 0;
-    else if(currentIndex >= elements.length - 5) currentIndex = elements.length - 5;
+// function scrollWatchListLeft() {
 
-    console.log(currentIndex);
+//     if (window.outerWidth < 1200) {
+//         currentIndex++;
+//         if (currentIndex >= elements.length) currentIndex--;
+//         scrollSize = elements[currentIndex].getBoundingClientRect().left;
+//         console.log(scrollSize);
+//     }
+//     else {
+//         currentIndex += 6;
+//         if (currentIndex >= elements.length) currentIndex = elements.length - 1;
+//         scrollSize = elements[currentIndex].getBoundingClientRect().left;
+//     }
 
-    scrollSize = elements[currentIndex].getBoundingClientRect().left
+//     el.scrollLeft += scrollSize;
+// }
 
-    el.scrollLeft += scrollSize;
-}
+// function scrollWatchListRight() {
+
+//     if (window.outerWidth < 1200) {
+//         currentIndex--;
+//         if (currentIndex < 0) currentIndex++;
+//         scrollSize = elements[currentIndex].getBoundingClientRect().left;
+//     }
+//     else {
+//         currentIndex -= 6;
+//         if (currentIndex < 0) currentIndex = 0;
+//         scrollSize = elements[currentIndex].getBoundingClientRect().left;
+//     }
+
+//     el.scrollLeft += scrollSize;
+// }
