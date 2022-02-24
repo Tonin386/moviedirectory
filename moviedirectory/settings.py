b562bc2aeec1d010a163754baa22be3e18671016
Antonin Mathubert <tonin386@gmail.com>
2022-02-24 22:46:43 +0100

    Changed to debug false!!

--- moviedirectory/settings.py
+++ moviedirectory/settings.py
@@ -27,7 +27,7 @@ config.read(BASE_DIR+'/config.ini')
 SECRET_KEY = config.get('django', 'secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['movie-directory.com']
 AUTH_USER_MODEL = 'moviedirectory.User'
